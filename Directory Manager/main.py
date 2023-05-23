082d7da8cb38cca51c68f5a885ba2fc02ffbb2aa
Astrow <88676947+Shnukle@users.noreply.github.com>
2023-05-22 21:53:31 -0400

    removed unimportant files and added feature
    removed reduntant files and added a function to list files in Directory

--- Directory Manager/main.py
+++ Directory Manager/main.py
@@ -1,10 +1,14 @@
 from pathlib import Path
-from dir_manage.dir_manager import delete_files
+from dir_manage import dir_manager
 
 usrinput = input("enter directory: ")
 
 path = Path(usrinput)
 
 if path.exists() == True:
-    delete_files(usrinput)
+    dir_manager.check_directory(usrinput)
+    confirm = input("are you sure you want to permanantly delete these files? [Y] [N] ")
+    if confirm == 'y':
+        dir_manager.delete_files(usrinput)
+    else: print("files unchanged")
 else: print("error: directory could not be found")
